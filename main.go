12bd36e6e09f7733c4fb0af305cb4809c837629e
bonjourclaudio <claudio.weckherlin@gmail.com>
2025-11-04 16:06:15 +0100

    only keep 5 entries

--- main.go
+++ main.go
@@ -183,7 +183,11 @@ func parseLive(url string) ([]Entry, error) {
 	})
 
 	if cur != nil && cur.title != "" {
+		// only keep 5 entries
 		entries = append(entries, finalize(*cur))
+		if len(entries) > 5 {
+			entries = entries[len(entries)-5:]
+		}
 	}
 
 	// Only keep well-formed entries
